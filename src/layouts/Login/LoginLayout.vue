c24be2417f1d9d1571d58b8ffb2e5f0efe72a6dc
dyunas <jonathan.almodiel.quebral@gmail.com>
2019-11-28 16:39:16 +0800

    Initial Commit

---
+++ src/layouts/Login/LoginLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass">
+body 
+  background-color: #fdff99
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,96 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="row">
+      <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    </div>
+    <div class="row">
+      <div class="q-pa-md">
+        <q-card class="my-card">
+          <q-card-section>
+            <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+              <q-input
+                dense
+                v-model="username"
+                label="Username"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type your username']"
+                autofocus
+              >
+                <template v-slot:prepend>
+                  <q-icon name="person" />
+                </template>
+              </q-input>
+
+              <q-input
+                dense
+                v-model="password"
+                :type="isPwd ? 'password' : 'text'"
+                label="Password"
+                lazy-rules
+                :rules="[
+                  val => val !== null && val.length > 0 || 'Please type your password',
+                ]"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                  />
+                </template>
+              </q-input>
+
+              <q-toggle v-model="remember" label="Remember me" />
+
+              <div>
+                <q-btn label="Submit" type="submit" color="primary" outline size="md" />
+                <q-btn
+                  label="Reset"
+                  type="reset"
+                  color="primary"
+                  class="q-ml-sm"
+                  outline
+                  size="md"
+                />
+              </div>
+            </q-form>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      remember: false,
+      isPwd: true
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      this.$router.push({ path: 'dashboard' })
+    },
+
+    onReset() {
+      this.username = ''
+      this.password = ''
+      this.remember = false
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.my-card
+  width: 100%
+  min-width: 300px
+</style>
